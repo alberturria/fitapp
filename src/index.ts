8267634c6a283c3b135eddb372dbae67c2d10356
Alberto Herrera <albertoherreravargas@gmail.com>
2023-03-26 12:32:52 +0200

    refactor: prettify the codebase

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ AppDataSource.initialize()
 		});
 
 		app.listen(port, () => {
-			console.log(`⚡️[server]: Server is running at http://localhost:${port ?? 4000}`);
+			console.log(
+				`⚡️[server]: Server is running at http://localhost:${
+					port ?? 4000
+				}`
+			);
 		});
 	})
 	.catch((error) => console.error(`Error initializing DB: ` + error));
--- src/migrations/1679825322198-add-user-model.ts
+++ src/migrations/1679825322198-add-user-model.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class addUserModel1679825322198 implements MigrationInterface {
-    name = 'addUserModel1679825322198'
+	name = "addUserModel1679825322198";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, "weight" integer, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, "weight" integer, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`DROP TABLE "user"`);
+	}
 }
