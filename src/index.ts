c654b8f385c579791a1d31b74d9b6a8820dfccd9
Alberto Herrera <albertoherreravargas@gmail.com>
2023-03-28 22:33:30 +0200

    refactor: split entrypoint and server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
-import express, { Express, Request, Response } from "express";
+import { Request, Response } from "express";
 import dotenv from "dotenv";
 import "reflect-metadata";
-import { AppDataSource } from "./data-source.js";
+import { AppDataSource } from "./data/data-sources/data-source.js";
+import server from "./server.js";
+import { userRouter } from "./presenter/user-router.js";
+import { GetAllUsersUseCase } from "./domain/use-cases/user/get-all-users-use-case.js";
+import { UserRepository } from "./domain/repositories/user-repository.js";
+import { UserDataSource } from "./data/data-sources/user-data-source.js";
 dotenv.config();
 
-AppDataSource.initialize()
-  .then(() => {
-    const app: Express = express();
-    const port = process.env.PORT;
-    app.get("/", (_req: Request, res: Response) => {
-      res.send("Express + TypeScript Server");
-    });
+(async () => {
+  AppDataSource.initialize()
+    .then((dataSource) => {
+      const port = process.env.PORT ?? 4000;
+      server.get("/", (_req: Request, res: Response) => {
+        console.log(" bbb");
+        res.send("Express + TypeScript Server");
+      });
 
-    app.listen(port, () => {
-      console.log(
-        `⚡️[server]: Server is running at http://localhost:${port ?? 4000}`
+      server.use(
+        "/users",
+        userRouter(
+          new GetAllUsersUseCase(
+            new UserRepository(new UserDataSource(dataSource))
+          )
+        )
       );
-    });
-  })
-  .catch((error) => console.error("Error initializing DB: " + error));
+      server.listen(port, () => {
+        console.log(
+          `⚡️[server]: Server is running at http://localhost:${port}`
+        );
+      });
+    })
+    .catch((error) => console.error("Error initializing DB: " + error));
+})();
